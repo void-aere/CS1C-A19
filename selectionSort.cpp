56eda66416e09a013314e30e494f9f18ba6ef548
Jacob Pratt <jpratt13@saddleback.edu>
2024-12-09 10:46:25 -0800

    actually implemented selection sort. doofus.

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
+
 void selectionSort(long long int A[], int n) {
-	/* TODO: Implement this function! */
-	(void) A;
-	(void) n;
-	return;
+	int mindex;
+	long long temp;
+	for (int i = 0; i < n - 1; ++i) {
+		mindex = i;
+		for (int j = i + 1; j < n; ++j) {
+			if (A[j] < A[mindex]) {
+				mindex = j;
+			}
+		}
+		if (mindex != i) {
+			temp = A[i];
+			A[i] = A[mindex];
+			A[mindex] = temp;
+		}
+	}
 }
